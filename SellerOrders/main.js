c33d856d23a1ab7c5913e3275543623cfeddc7c7
August Meyer <ameyer5@luc.edu>
2015-12-07 02:12:03 -0600

    Added Seller Signup functionality

--- SellerOrders/main.js
+++ SellerOrders/main.js
@@ -40,6 +40,31 @@
     });
 }
 
+function getSellerReview(){
+      $.ajax({
+      url: sellerOrders,
+      method: 'GET',
+    }).done(function(data) {
+      var i;
+      var table="<table class='table'><tr><th>ID</th><th>Product Name</th><th>Order Status</th><th>Fulfill?</th></tr>";
+      var x = data.getElementsByTagName("Order");
+      for (i = 0; i <x.length; i++) {
+        var orderLinkNode = x[i].getElementsByTagName("link");
+        var orderURL = orderLinkNode[2].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+        console.log(orderURL);
+        var button = '<button type="submit" name="submit" class="btn btn-default" onClick="fulfillOrder(\'' + orderURL + '\')">Fulfill My Order</button>';
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("id")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("productName")[0].childNodes[0].nodeValue
+        + "</td><td>" +
+        x[i].getElementsByTagName("status")[0].childNodes[0].nodeValue
+        + "</td><td>" + button
+      }
+      document.getElementById("orderTable").innerHTML = table + "</table>";
+    });
+}
+
 function fulfillOrder(orderURL){
   $.get(orderURL, function(data){
     console.log("Order " + orderURL + " fulfilled!");
