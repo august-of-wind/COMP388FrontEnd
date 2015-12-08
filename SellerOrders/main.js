71779b504783d30b123e89fdbd89ea83235cd31a
August Meyer <august@Augusts-MacBook-Air.local>
2015-12-07 22:42:37 -0600

    Added Reviews and Products tables to Seller Homepage.

--- SellerOrders/main.js
+++ SellerOrders/main.js
@@ -10,6 +10,7 @@
       updatePwdLink = links[1].getElementsByTagName("url")[0].childNodes[0].nodeValue;
       sellerReviews = links[2].getElementsByTagName("url")[0].childNodes[0].nodeValue;
       sellerOrders = links[3].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+      sellerProducts = links[4].getElementsByTagName("url")[0].childNodes[0].nodeValue;
       document.getElementById("sellerName").innerHTML = "Welcome " + sellerName + "!";
     })
   });
@@ -36,32 +37,52 @@
         x[i].getElementsByTagName("status")[0].childNodes[0].nodeValue
         + "</td><td>" + button
       }
-      document.getElementById("orderTable").innerHTML = table + "</table>";
+      document.getElementById("sellerTable").innerHTML = table + "</table>";
     });
 }
 
-function getSellerReview(){
+function getProducts(){
+
       $.ajax({
-      url: sellerOrders,
+      url: sellerProducts,
       method: 'GET',
     }).done(function(data) {
       var i;
-      var table="<table class='table'><tr><th>ID</th><th>Product Name</th><th>Order Status</th><th>Fulfill?</th></tr>";
-      var x = data.getElementsByTagName("Order");
+      var table="<table class='table'><tr><th>ID</th><th>Product Name</th><th>Product Price</th></tr>";
+      var x = data.getElementsByTagName("Product");
       for (i = 0; i <x.length; i++) {
-        var orderLinkNode = x[i].getElementsByTagName("link");
-        var orderURL = orderLinkNode[2].getElementsByTagName("url")[0].childNodes[0].nodeValue;
-        console.log(orderURL);
-        var button = '<button type="submit" name="submit" class="btn btn-default" onClick="fulfillOrder(\'' + orderURL + '\')">Fulfill My Order</button>';
         table += "<tr><td>" +
         x[i].getElementsByTagName("id")[0].childNodes[0].nodeValue +
         "</td><td>" +
-        x[i].getElementsByTagName("productName")[0].childNodes[0].nodeValue
+        x[i].getElementsByTagName("productDetail")[0].childNodes[0].nodeValue
         + "</td><td>" +
-        x[i].getElementsByTagName("status")[0].childNodes[0].nodeValue
-        + "</td><td>" + button
+        x[i].getElementsByTagName("productPrice")[0].childNodes[0].nodeValue
+        + "</td><td>"
       }
-      document.getElementById("orderTable").innerHTML = table + "</table>";
+      document.getElementById("sellerTable").innerHTML = table + "</table>";
+    });
+}
+
+function getReviews(){
+
+      $.ajax({
+      url: sellerReviews,
+      method: 'GET',
+    }).done(function(data) {
+      var i;
+      var table="<table class='table'><tr><th>Review</th><th>Customer Name</th><th>Rating (out of 5)</th></tr>";
+      var x = data.getElementsByTagName("Review");
+      for (i = 0; i <x.length; i++) {
+        //console.log(reviewURL);
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("reviewDetail")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("customerName")[0].childNodes[0].nodeValue
+        + "</td><td>" +
+        x[i].getElementsByTagName("rating")[0].childNodes[0].nodeValue
+        + "</td><td>"
+      }
+      document.getElementById("sellerTable").innerHTML = table + "</table>";
     });
 }
 
